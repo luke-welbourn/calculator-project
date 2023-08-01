2f71e42d42e7026272483f81f30010d996b60417
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-01 15:12:09 +0100

    created own file for .lightMode, dark mode image now equals the light side image, operators are styled as in template, main screen can handle multiple digits.

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,7 @@ const updateDisplay = (value: string) => {
 const clearDisplay = () => {
   sumHistory.length = 0;
   sumHistoryDisplay.innerText = "";
+  currentNumber = "";
 };
 
 function calculateSum() {
@@ -103,6 +104,24 @@ function calculateSum() {
 //
 //
 //
+let currentNumber = "";
+
+const showOnScreen = (value: string) => {
+  const validOperators = ["+", "-", "*", "/"];
+  const validNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  if (value === "." && currentNumber.includes(".")) {
+    return;
+  }
+
+  if (validOperators.includes(value)) {
+    screen.innerText = value;
+    currentNumber = "";
+  } else if (validNumbers.includes(value) || value === ".") {
+    currentNumber += value;
+    screen.innerText = currentNumber;
+  }
+};
 
 buttons.forEach((button) => {
   button.addEventListener(`click`, () => {
@@ -118,52 +137,52 @@ buttons.forEach((button) => {
       screen.innerText = `%`;
       updateDisplay(value);
     } else if (value === "/") {
-      screen.innerText = `/`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "7") {
-      screen.innerText = `7`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "8") {
-      screen.innerText = `8`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "9") {
-      screen.innerText = `9`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "*") {
-      screen.innerText = `*`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "4") {
-      screen.innerText = `4`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "5") {
-      screen.innerText = `5`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "6") {
-      screen.innerText = `6`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "-") {
-      screen.innerText = `-`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "1") {
-      screen.innerText = `1`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "2") {
-      screen.innerText = `2`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "3") {
-      screen.innerText = `3`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "+") {
-      screen.innerText = `+`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "reverse") {
       screen.innerText = `undo`;
       updateDisplay(value);
     } else if (value === "0") {
-      screen.innerText = `0`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === ".") {
-      screen.innerText = `.`;
+      showOnScreen(value);
       updateDisplay(value);
     } else if (value === "=") {
       const result = calculateSum();
